75559e5a50644dec9ddb5848dc07463965499c84
Ahmed Afify Ahmed <168142238+AHMED-AFIFY8@users.noreply.github.com>
2026-03-10 01:58:12 +0200

    Create main.dart

---
+++ Task_01/main.dart
@@ -0,0 +1,22 @@
+void main() {
+int degree = 99;
+if (degree >= 95) {
+    print('Degree: +A');
+  } else if (degree >= 90) {
+    print('Degree: -A');
+  } else if (degree >= 85) {
+    print('Degree: +B');
+  } else if (degree >= 80) {
+    print('Degree: -B');
+  } else if (degree >= 75) {
+    print('Degree: +C');
+  } else if (degree >= 70) {
+    print('Degree: -C');
+} else if (degree >= 65) {
+    print('Degree: +D');
+  } else if (degree >= 60) {
+    print('Degree: -D');
+  } else {
+    print('Degree: F');
+  }
+}
